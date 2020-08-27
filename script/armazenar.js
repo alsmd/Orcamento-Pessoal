250900ccefddbd38e4c6660f152b08125e069951
Celebro01 <dangerveneta@gmail.com>
2020-08-27 13:24:38 -0300

    Chaves dinamicas  no localStorage

--- script/armazenar.js
+++ script/armazenar.js
@@ -13,6 +13,33 @@
 
 
     }
+    class Bd {
+
+        constructor(){
+            let id = localStorage.getItem('id')
+
+            if(id === null){
+                localStorage.setItem('id',0)
+            }
+            
+        }
+
+
+        getNextId() {
+            let proximoId = localStorage.getItem("id")
+            return parseInt(proximoId) + 1
+        }
+
+        gravar(d){
+            let id = this.getNextId()
+            localStorage.setItem(id,JSON.stringify(d))
+
+            localStorage.setItem('id',id)
+        }
+    }
+
+    let bd = new Bd();
+
 
     let btn = document.querySelector("#btn");
     btn.addEventListener('click',cadastrarDespesa)
@@ -25,14 +52,10 @@
         let descricao = document.querySelector("#descricao");
         let valor = document.querySelector("#valor");
         let despesa = new Despesa(ano.value,mes.value,dia.value,tipo.value,descricao.value,valor.value);
-        console.log(despesa.descricao)
-        gravar(despesa);
+        
+        bd.gravar(despesa);
     }
 
-    function gravar(d){
-        let chave = d.descricao;
-        localStorage.setItem(chave,JSON.stringify(d))
-    }
 
 
 
